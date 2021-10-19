940f9e19139ef8f44050597856cb90393d76f6a4
sslanec <561069+sslanec@users.noreply.github.com>
2021-10-19 14:28:54 -0400

    Fixed global badge retrieval

--- src/utils/api/getGlobalBadges.js
+++ src/utils/api/getGlobalBadges.js
@@ -19,15 +19,15 @@ export default async function getGlobalBadges(apiClient) {
   await apiClient.helix.chat.getGlobalBadges().then(data => {
     for (var i in data) {
       var ver = {};
-      for (var j in data[i]['_data']['versions']) {
-        ver[data[i]['_data']['versions'][j]['id']] = {};
+      for (var j in data[i]['versions']) {
+        ver[data[i]['versions'][j]['id']] = {};
 
-        ver[data[i]['_data']['versions'][j]['id']]['1x'] =
-          data[i]['_data']['versions'][j]['image_url_1x'];
-        ver[data[i]['_data']['versions'][j]['id']]['2x'] =
-          data[i]['_data']['versions'][j]['image_url_2x'];
-        ver[data[i]['_data']['versions'][j]['id']]['3x'] =
-          data[i]['_data']['versions'][j]['image_url_4x'];
+        ver[data[i]['versions'][j]['id']]['1x'] =
+          data[i]['versions'][j].getImageUrl(1);
+        ver[data[i]['versions'][j]['id']]['2x'] =
+          data[i]['versions'][j].getImageUrl(2);
+        ver[data[i]['versions'][j]['id']]['3x'] =
+          data[i]['versions'][j].getImageUrl(4);
       }
       badges[data[i]['id']] = ver;
     }
