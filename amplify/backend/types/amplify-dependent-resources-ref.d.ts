89e1654d5b8136cd390f149d2e8f6417b5c59ef5
sslanec <561069+sslanec@users.noreply.github.com>
2022-01-24 11:34:02 -0500

    Migrated API resources to new version

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "TwitchLogin": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "TwitchLogin": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
